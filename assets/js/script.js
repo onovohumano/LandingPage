935169b96af345648bee5643703f3fc2a50f607a
Fabio Muller <fabio.muller@fael.edu.br>
2020-05-18 11:26:23 -0300

    Html basico

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", onExitPage, false);
+// let oneTime = true
+let startTime = 0
+let endTime = 0
+let seconds = 0
+
+function onExitPage() { 
+  let el = document.documentElement
+  el.addEventListener("mouseleave", openMessage, false);
+}
+
+function openMessage() {
+  end()
+  console.log(seconds + " seconds");
+
+  if(seconds == 0 || seconds >= 10){
+    // $('#exampleModal').modal('show')
+    alert("Modal")
+    start()
+  }
+}
+
+function start() {
+  startTime = new Date();
+};
+
+function end() {
+  if(startTime > 0) {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    // strip the ms
+    timeDiff /= 1000;
+    // get seconds 
+    seconds = Math.round(timeDiff);
+    console.log(seconds + " seconds");
+  }
+}
